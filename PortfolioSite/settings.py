c94cefa6811027bad9fbd4a380e62c2ba0fd61a6
Erik Butcher <butcer0@gmail.com>
2020-06-06 05:07:12 -0400

    Introduced Blog DataModel and Updated AWS RDS Schema
    Introduced Blog DataModel and Updated AWS RDS Schema

--- PortfolioSite/settings.py
+++ PortfolioSite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio.apps.PortfolioConfig',
-    'jobs.apps.JobsConfig'
+    'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
