2c7b34a6dfb9ae596822fe1cb3ca9015ff506707
Erik Butcher <butcer0@gmail.com>
2020-06-07 01:36:45 -0400

    Updated Jobs html to loop jobs form database
    Updated Jobs html to loop jobs form database

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+from .models import Job
+
 
 def home(request):
-    return render(request, 'jobs/home.html')
-
+    jobs = Job.objects  # this gets every job and turns them into objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
