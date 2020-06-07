4229dcc37d357d3855dfc8ae8adc2d7bf777b7bb
Erik Butcher <butcer0@gmail.com>
2020-06-07 02:10:50 -0400

    Updated blogs application
    Updated blogs application

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,14 @@ class Blog(models.Model):
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    # the admin page uses this value to for the model label
+    def __str__(self):
+        return self.title
+
+    # functions are called the same as a model field
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
